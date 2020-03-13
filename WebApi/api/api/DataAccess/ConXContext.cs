adab77e8bdf45be19aae495c2f2522e241be7989
Harudee <harudee@hotmail.com>
2020-03-13 15:26:19 +0700

    api

---
+++ WebApi/api/api/DataAccess/ConXContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace api.DataAccess
+{
+    public class ConXContext : DbContext
+    {
+        public ConXContext() : base("ConXContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        //public DbSet<Token> Tokens { get; set; }
+        //public DbSet<BranchGroup> BranchGroups { get; set; }
+        //public DbSet<Branch> Branchs { get; set; }
+        //public DbSet<Department> Departments { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<UserStatus> UserStatus { get; set; }
+        //public DbSet<UserRole> UserRoles { get; set; }
+        //public DbSet<UserRoleFunctionAuthorization> UserRoleFunctionAuthorizations { get; set; }
+        //public DbSet<UserRoleFunctionAccess> UserRoleFunctionAccesses { get; set; }
+        //public DbSet<UserBranchGroupPrvlg> UserBranchGroupPrvlgs { get; set; }
+        //public DbSet<UserBranchPrvlg> UserBranchPrvlgs { get; set; }
+        //public DbSet<MenuFunctionGroup> MenuFunctionGroups { get; set; }
+        //public DbSet<MenuFunction> MenuFunctions { get; set; }
+        //public DbSet<MenuFunctionAction> MenuFunctionAction { get; set; }
+
+        //public DbSet<InformationMessage> InformationMessages { get; set; }
+
+        
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+
+            // Set Quantity Precision ===================================================
+
+            //modelBuilder.Entity<Item>().Property(r => r.minimumStockQty).HasPrecision(18, 4);
+
+            //=========================================================================
+
+
+            // Create Index ===========================================================
+
+            //=== Branch Group ========================================================
+            //modelBuilder.Entity<BranchGroup>()
+            //    .HasIndex("IX_BranchGroup_BranchGroupCode",
+            //    e => e.Property(x => x.branchGroupCode));
+
+            //modelBuilder.Entity<Branch>()
+            //    .HasIndex("IX_Branch_BranchCode",
+            //    e => e.Property(x => x.branchCode));
+
+            //modelBuilder.Entity<Department>()
+            //    .HasIndex("IX_Department_DepartmentCode",
+            //    e => e.Property(x => x.departmentCode));
+
+
+
+
+
+
+            //=========================================================================
+
+        }
+    }
+}
